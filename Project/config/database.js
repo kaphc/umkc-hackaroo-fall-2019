cac5cfb636a954403ee5f5980b922b753db9e010
Kavin Kumar Arumuagm <42685150+kaphc@users.noreply.github.com>
2019-11-10 16:37:00 -0600

    initial

---
+++ Project/config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  database: 'mongodb://Hiresh12:Hraccount2k19@ds151383.mlab.com:51383/home_security',   //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'HANDR'
+}
